c33ec6e45f9761a4fe9ef64ae75f5097e44303f3
jiangli <Jillian0210>
2021-01-10 21:09:08 +0800

    文件读取

--- main6.c
+++ main6.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void writeTest();
+
+void readTest();
+
+void copyTest();
+
+void huanhangTest();
+
+void byteFileTest();
+
 //int * 不可以换成int[] ,因为在c中int[] 是常量，不能进行赋值。所以，c中，数组不能作为函数的返回值
 int *test1() {
     int a[3] = {1, 2, 3};
@@ -22,28 +32,96 @@ int main() {
 // 虽然函数的类型一样，但是无法保持函数所占用的空间相同。
 //    int fun1[5](void);
 
-//c--->.o---->.exe 文件
-    printf("file write ——————————————————————————\n");
-    //写文件 w一定是小写的  以二进值的方式则加 wb
-    FILE *p = fopen("./test/write.txt", "w");
-    if (p) {
-        printf("需要写入的文件存在\n");
-        fputc('a', p);
-        fputc("bbbb", p);
-        //文件存在
-        fclose(p);
-        //p 悬在指针 内存被销毁了
-        //所以一定要改为NULL
-        p = NULL;
+    writeTest();
+    readTest();
+    copyTest();
+    huanhangTest();
 
-    } else {
-        //文件不存在
-        printf("需要写的文件不存在\n");
+    return 0;
+}
+
+void  readByteFile(int argc,char **argv){
+
+}
+
+int cal(int a,char b,int c){
+    int  result=0;
+    switch (b) {
+        case '+':
+            result=a+c;
+            break;
+        case '-':
+            result=a-c;
+            break;
+        case '*':
+            result=a*c;
+            break;
+        case '/':
+            if (c==0){
+                printf("被除数不能为0");
+            }else{
+                result= a/c;
+            }
+            break;
+    }
+    return result;
+}
+void huanhangTest() {
+    //读取需要换行的文件,使用缓冲区去读
+    printf("file buffer————————————————————————\n");
+    //注意模式一定是"r"，不能是"w"，否则会直接清空huanghang.txt 文件的内容
+    FILE *p = fopen("./../test/huanhang.txt", "r");
+    if (p) {
+        char buffer[100] = {0};
+        while (1) {
+            //从文件中读取一行，  放在buffer缓冲区中，
+            fgets(buffer, sizeof(buffer), p);
+            if (feof(p)) {//用于判断文件是否到达了末尾
+                break;
+            }
+
+            int a = 0;
+            char b = 0;
+            int c = 0;
+            //格式化读取
+            sscanf(buffer, "%d%c%d", &a, &b, &c);
+            printf("%d%c%d=%d\n", a, b, c,cal(a,b,c));
+        }
+        fclose(p);
+        p = NULL;
     }
 
 
+}
+
+void copyTest() {
+
+    printf("file copy ——————————————————————————\n");
+    FILE *sourceP = fopen("./../test/source.txt", "r");
+    FILE *targetP = fopen("./../test/target.txt", "w");
+    if (sourceP == NULL || targetP == NULL) {
+        return;
+    }
+    while (1) {
+        char c = fgetc(sourceP);
+        if (c == EOF) {
+            printf("copy complete\n");
+            break;
+        }
+        fputc(c, targetP);
+    }
+    //注意一定要关闭文件，释放指针
+    fclose(sourceP);
+    fclose(targetP);
+    sourceP = NULL;
+    targetP = NULL;
+
+
+}
+
+void readTest() {
     printf("file read —————————————————————————\n");
-    FILE *p1 = fopen("./test/read.txt", "r");
+    FILE *p1 = fopen("./../test/read.txt", "r");
     if (p1) {
         printf("需要读出的文件存在\n");
         while (1) {
@@ -64,51 +142,32 @@ int main() {
     p1 = NULL;
 
 
-    printf("file copy ——————————————————————————\n");
-    FILE *sourceP = fopen("./test/source.txt", "r");
-    FILE *targetP = fopen("./test/target.txt", "w");
-    if (sourceP == NULL || targetP == NULL) {
-        return -1;
-    }
-    while (1) {
-        char c = fgetc(sourceP);
-        if (c == EOF) {
-            printf("copy complete\n");
-            break;
-        }
-        fputc(c, targetP);
-    }
-    //注意一定要关闭文件，释放指针
-    fclose(sourceP);
-    fclose(targetP);
-    sourceP = NULL;
-    targetP = NULL;
+}
 
+void writeTest() {
+//c--->.o---->.exe 文件
+    printf("file write ——————————————————————————\n");
+    //写文件 w一定是小写的  以二进值的方式则加 wb
+    //相对路径 用正斜杠(除法用的斜杠)
+    //绝对路径 用反斜杠
+    //路径可以解释为：
+    //   ./  当前路径（cmake-build-debug）
+    //   ../ 当前路径的上一个路径 （cDemo）
+    //    cDemo/test/write.txt
+    FILE *p = fopen("./../test/write.txt", "w");
+    if (p) {
+        printf("需要写入的文件存在\n");
+        fputc('a', p);
+        //文件存在
+        fclose(p);
+        //p 悬在指针 内存被销毁了
+        //所以一定要改为NULL
+        p = NULL;
 
-    //读取需要换行的文件,使用缓冲区去读
-    printf("file buffer————————————————————————");
-    FILE *huanhangP = fopen("./test/huanhang.txt", "w");
-    if (huanhangP) {
-        char buffer[100] = {0};
-        while (1) {
-            //从文件中读取一行，放在buffer缓冲区中，
-            fgets(buffer, sizeof(buffer), huanhangP);
-
-            if (feof(huanhangP)) {//用于判断文件是否到达了末尾
-                break;
-            }
-
-            int a = 0;
-            char b = 0;
-            int c = 0;
-            //格式化读取
-//            fscanf(buffer,"%d %c %d",&a,&b,&c);
-            printf("a %d b %c c %d", a, b, c);
-        }
-        fclose(huanhangP);
-        huanhangP = NULL;
+        printf("写入完成，查看write.txt 文件\n");
+    } else {
+        //文件不存在
+        printf("需要写的文件不存在\n");
     }
 
-
-    return 0;
 }
--- main7.c
+++ main7.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 
-int main() {
+int main(int agrc, char **argv) {
+
+    //    argv[0]
     printf("file write \n");
-    FILE *p=fopen("./test/maniu.txt","");
+    char *path1 = "./../test/input.mp4";
+    FILE *pr = fopen(path1, "rb");
+//    printf("path1=%s",argv[1]);
+    if (pr == NULL) return 0;
+
+    char *path2 = "./../test/output.mp4";
+    FILE *pw = fopen(path2, "wb");
+//    printf("path2=%s",argv[2]);
+    if (pw == NULL) return 0;
+
+    printf("copy");
+
+
+
+
     return 0;
 }
