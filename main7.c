4532fea8f354f11df7c194e07d9e34bf615380b0
jiangli <Jillian0210>
2021-01-13 19:15:47 +0800

    文件读写 byte 方式

--- main7.c
+++ main7.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+//stat 结构体，获取文件的大小
+#include <sys/stat.h>
 
+//64k 每次读取的文件的大小
+#define BLOCK_SIZE 1024*64
 
 int main(int agrc, char **argv) {
 
@@ -16,10 +21,32 @@ int main(int agrc, char **argv) {
 //    printf("path2=%s",argv[2]);
     if (pw == NULL) return 0;
 
-    printf("copy");
 
+    struct stat st = {0};
+    stat(path1, &st);
+    //获取文件的大小
+    int size = st.st_size;
+    printf("file size %d\n", size);
+    if (size > BLOCK_SIZE) {
+        //如果文件的太大，分为多次进行读写
+        size = BLOCK_SIZE;
+    }
+    //malloc 内存空间不会设置为0
+    //calloc 内存会设置为0
+    char *buf = calloc(1, size);
+    unsigned int index = 0;
+    while (!feof(pr)) {
+        int res = fread(buf, 1, size, pr);
+        fwrite(buf, 1, res, pw);
+        index++;
+    }
+    printf("file read count=%d\n",index);
 
-
+    free(buf);
+    fclose(pw);
+    fclose(pr);
+    pw=NULL;
+    pr=NULL;
 
     return 0;
 }
